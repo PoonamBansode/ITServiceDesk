b5d7be8b1a5ebca20385737e8225f9a437f64f18
PoonamBansode <41625383+PoonamBansode@users.noreply.github.com>
2018-07-24 16:46:52 +0530

    Initial Commit

---
+++ publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild Microsoft.Bot.Sample.LuisBot.sln -p:DeployOnBuild=true -p:PublishProfile=itservicedeskskype-Web-Deploy.pubxml -p:Password=EM88yf6wSZpiJoXEsDBby7JHnRvugyKzc8qmhkThn9WHg2QWMThElBFk6hex
+
